aa5326053dc6ba067a51701c874dfc071ee6cec1
Bryanprestige <bryanhvaldez@gmail.com>
2025-04-06 18:00:10 +0200

    feat: changed prompt
    changing prompt

--- src/ai.js
+++ src/ai.js
@@ -3,9 +3,16 @@ import Anthropic from "@anthropic-ai/sdk"
 
 const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients.
-You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to 
-include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
+You don't need to use every ingredient they mention in your recipe. The recipe can include maximun 3 additional ingredients they didn't mention. Avoid using ingredients that wouldn't be available at a usual store.
+Format your response in markdown to make it easier to render to a web page
 `
+
+
+/*ADD 3 DIFFERENT RECIPIES SHOWN ON THE PAGE*/ 
+/*WHEN THE USER CLICKS ON THE RECIPE, IT SHOULD SHOW THE RECIPE,AND HIDE THE OTHER OPTIONS, ALSO A HIDE RECIPE BUTTON SHOULD SHOW*/ 
+/*USER CAN EDIT ITS DECISION BY ADDING OR REMOVING INGREDIENTS*/ 
+/**/ 
+
 
 /*=====ANTHROPIC API =====*/
 
@@ -23,7 +30,7 @@ export async function getRecipeFromChefClaude(ingredientsArr) {
         max_tokens: 1024,
         system: SYSTEM_PROMPT,
         messages: [
-            { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
+            { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!, i want to make something with` },
         ],
     });
     return msg.content[0].text
